688bd88c4e5623b43499eb2ae60dfb03fdb72c33
Killar.exe <killar.exe@gmail.com>
2024-02-10 15:51:24 +0100

    Removed code

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const menu = document.getElementById("menu");
-const background = document.getElementById("menu-background-pattern");
-
-Array.from(document.getElementsByClassName("menu-item")).forEach((item, index) => {
-  item.onmouseover = () => {
-    background.style.backgroundPosition = "0% " + (index * -25 + -25) + "%";
-  }
-});
